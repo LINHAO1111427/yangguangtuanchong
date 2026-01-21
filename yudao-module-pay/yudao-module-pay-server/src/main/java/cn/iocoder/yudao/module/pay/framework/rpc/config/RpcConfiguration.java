08e927cd5db3e6111d23689590f9aa6bf8148d91
a745839lin <18602236204@163.com>
2026-01-21 23:00:59 +0800

    fix(pay): enable member user feign client

--- yudao-module-pay/yudao-module-pay-server/src/main/java/cn/iocoder/yudao/module/pay/framework/rpc/config/RpcConfiguration.java
+++ yudao-module-pay/yudao-module-pay-server/src/main/java/cn/iocoder/yudao/module/pay/framework/rpc/config/RpcConfiguration.java
@@ -1,10 +1,11 @@
 package cn.iocoder.yudao.module.pay.framework.rpc.config;
 
+import cn.iocoder.yudao.module.member.api.user.MemberUserApi;
 import cn.iocoder.yudao.module.system.api.social.SocialClientApi;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(value = "payRpcConfiguration", proxyBeanMethods = false)
-@EnableFeignClients(clients = {SocialClientApi.class})
+@EnableFeignClients(clients = {SocialClientApi.class, MemberUserApi.class})
 public class RpcConfiguration {
 }
