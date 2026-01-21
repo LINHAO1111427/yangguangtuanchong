7325ef108f3478bbb8c33a437bcee0660e9f269e
a745839lin <18602236204@163.com>
2026-01-21 23:12:13 +0800

    fix(pay): enable admin user feign client

--- yudao-module-pay/yudao-module-pay-server/src/main/java/cn/iocoder/yudao/module/pay/framework/rpc/config/RpcConfiguration.java
+++ yudao-module-pay/yudao-module-pay-server/src/main/java/cn/iocoder/yudao/module/pay/framework/rpc/config/RpcConfiguration.java
@@ -1,11 +1,12 @@
 package cn.iocoder.yudao.module.pay.framework.rpc.config;
 
 import cn.iocoder.yudao.module.member.api.user.MemberUserApi;
+import cn.iocoder.yudao.module.system.api.user.AdminUserApi;
 import cn.iocoder.yudao.module.system.api.social.SocialClientApi;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(value = "payRpcConfiguration", proxyBeanMethods = false)
-@EnableFeignClients(clients = {SocialClientApi.class, MemberUserApi.class})
+@EnableFeignClients(clients = {SocialClientApi.class, MemberUserApi.class, AdminUserApi.class})
 public class RpcConfiguration {
 }
